49c6c7e787a8c868ffc134fa0ae8becd8e8c3d01
Mart510 <martynandrewbillings@gmail.com>
2023-09-18 15:57:44 +0100

    Ticket 3 complete

--- ticket-3/index.js
+++ ticket-3/index.js
@@ -1,12 +1,17 @@
 function countNumberOfNinesInNumber(number) {
   let count = 0;
 
-  for (let i = 0; i < number.length; i++) {
-    const digit = number[i];
+  for (let i = 0; i < String(number).length; i++) {
+    let digit = String(number)[i];
+    // console.log(`i is ${i}`);
+    // console.log(`digit is ${digit}`);
+    // console.log(`number i is ${number[i]}`)
     if ("9" === digit) {
       count++;
     }
   }
-
+  
   return count;
 }
+// console.log('is this running?') 
+console.log(countNumberOfNinesInNumber(1115.1257846))
