5ee4e4afd2fb15a6c6dc81cd18c95fe783938f2f
Thomas Chappell <tom.chappell.info@gmail.com>
2023-09-18 16:40:12 +0100

    ticket 5 completed

--- ticket-5/index.js
+++ ticket-5/index.js
@@ -19,9 +19,9 @@ const shoppingCart = [
 
 let totalCost = 0;
 
-for (let i = 0; i <= shoppingCart.length; i++) {
+for (let i = 0; i < shoppingCart.length; i++) {
   const item = shoppingCart[i];
-  const costForItem = item.price * item.quantity;
+  const costForItem = item["price"] * item["quantity"];
   totalCost += costForItem;
 }
 
